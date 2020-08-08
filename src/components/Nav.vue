5ee88a432ff246452b43d7f21501b519c000cb4f
SilverAsh <qingfeng26@foxmail.com>
2020-08-08 15:29:19 +0800

    添加全局样式和变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
-      <Icon name="money"/>
-      记账
-    </router-link>
-    |
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics" class="item">
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -25,6 +23,7 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -40,6 +39,9 @@
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected{
+      color: $color-highlight;
     }
   }
 
